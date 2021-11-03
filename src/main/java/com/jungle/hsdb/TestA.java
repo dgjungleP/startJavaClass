6158acb4f246a6e5f354e77390a50150359bfd30
jungle <dgjungle_c@163.com>
2021-11-03 22:56:55 +0800

    use HSDB 查看JVM运行详情

---
+++ src/main/java/com/jungle/hsdb/TestA.java
@@ -0,0 +1,9 @@
+package com.jungle.hsdb;
+
+public abstract class TestA {
+    public void printMe() {
+        System.out.println("I`m A");
+    }
+
+    public abstract void sayHello();
+}
---
+++ src/main/java/com/jungle/hsdb/TestB.java
@@ -0,0 +1,8 @@
+package com.jungle.hsdb;
+
+public class TestB extends TestA {
+    @Override
+    public void sayHello() {
+        System.out.println("I`m B");
+    }
+}
