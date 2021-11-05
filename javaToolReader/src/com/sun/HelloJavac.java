d470cbbe8566d251fbcf5705f7e8f16d971870cb
jungle <dgjungle_c@163.com>
2021-11-05 22:54:22 +0800

    搭建 javac 源码阅读环境

--- javaToolReader/src/com/sun/HelloJavac.java
+++ javaToolReader/src/com/sun/HelloJavac.java
@@ -1,5 +1,9 @@
 package com.sun;
 
+import com.sun.tools.javac.parser.Scanner;
+import com.sun.tools.javac.parser.ScannerFactory;
+import com.sun.tools.javac.util.Context;
+
 public class HelloJavac {
     public static void foo() {
         int a = 0;
@@ -7,4 +11,24 @@ public class HelloJavac {
         int c = 130;
         int d = 33000;
     }
+
+    public static void main(String[] args) {
+        ScannerFactory factory = ScannerFactory.instance(new Context());
+        Scanner scanner = factory.newScanner(" int k = i + j;", false);
+        scanner.nextToken();
+        System.out.println(scanner.token().kind);
+        scanner.nextToken();
+        System.out.println(scanner.token().name());
+        scanner.nextToken();
+        System.out.println(scanner.token().kind);
+        scanner.nextToken();
+        System.out.println(scanner.token().name());
+        scanner.nextToken();
+        System.out.println(scanner.token().kind);
+        scanner.nextToken();
+        System.out.println(scanner.token().name());
+        scanner.nextToken();
+        System.out.println(scanner.token().kind);
+    }
+
 }
